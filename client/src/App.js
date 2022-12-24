21ea4238fd23ef4640c0e431c3fa5438ed58b7c8
shafiqullah turkmen <shafiqullahturkmen@gmail.com>
2022-12-24 20:08:57 +0300

    h1 added

--- client/src/App.js
+++ client/src/App.js
@@ -19,6 +19,7 @@ function App() {
           >
             Learn React
           </a>
+          <h1 className='App-title'>Fib Calculator</h1>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
         </header>
